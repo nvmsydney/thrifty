4dfd8559edf3850390520339532666eecbe8a9d3
jacksonja13 <jacksonja13@vcu.edu>
2024-04-23 03:32:28 -0400

    added login box and some more configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import StylePreview from "./components/StylePreview";
 import Footer from "./components/Footer";
@@ -5,18 +6,30 @@ import PopularDesigners from "./components/PopularDesigners";
 import HomeMenswear from "./components/HomeMenswear";
 import HomeWomenswear from "./components/HomeWomenswear";
 import ShopSellPost from "./components/ShopSellPost";
+import LoginBox from "./components/LoginBox";
+
 
 function App() {
   return (
-    <div className="d-flex flex-column h-100">
-      <NavBar />
-      <StylePreview />
-      <PopularDesigners />
-      <HomeMenswear />
-      <HomeWomenswear />
-      <ShopSellPost />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="d-flex flex-column h-100">
+        <NavBar />
+        <Routes>
+          <Route path="/~24SP_jacksonja13" element ={<LoginBox />} />
+          <Route path="/home" element={
+            <div className="content">
+              <StylePreview />
+              <PopularDesigners />
+              <HomeMenswear />
+              <HomeWomenswear />
+              <ShopSellPost />
+            </div>
+          }/>
+
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
