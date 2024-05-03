f2eedea36185cd23b8365162e00ab5d7df6e6181
Sydney Nguyen <melosydrose@gmail.com>
2024-05-03 07:12:32 -0400

    Clean up imports

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import { IoBagOutline } from "react-icons/io5";
 import { LinkContainer } from "react-router-bootstrap";
 
